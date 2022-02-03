923fb7cc89df50a0dc325648275ed3c10e4f9243
Hannah Glazier <hrayglazier@aol.com>
2022-02-02 17:19:02 -0700

    commit tree 7640b65421ca8a3e3ba337e79c1072008382f625

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Code your solution here
+
+// Array of drivers
+//const drivers = ['Fred', 'Jim', 'Helen', 'Rachel', 'Dan']
+
+function findMatching (drivers, string) {
+    return drivers.filter(function(element){
+        return element.toLowerCase() === string.toLowerCase() 
+    })
+}
+
+//console.log(findMatching(drivers, "Jim"))
+
+function fuzzyMatch (drivers, string) {
+    return drivers.filter(function(element){
+        return element.charAt(0) === string.charAt(0)
+    })
+}
+
+
+
+    //console.log(fuzzyMatch(drivers, "H"))
+
+// Array of driver objects 
+const driverObj = [
+{
+    name: 'Fred',
+    hometown: 'Portland',
+},
+{    
+    name: 'Jim',
+    hometown: 'Boulder',
+},
+{
+    name: 'Helen', 
+    hometown: 'Denver',
+},
+{
+    name: 'Rachel',
+    hometown: 'Seattle',
+},
+{
+    name: 'Dan',
+    hometown: 'San Diego'
+}   
+]
+
+
+function matchName (driverObj, string) {
+    return driverObj.filter(driverObj => driverObj.name === string)
+}
